ce21f06a4d3ce759ed959691abc4e09613a4eacb
steft <52441940+stef-t@users.noreply.github.com>
2021-07-19 14:23:29 +0200

    initial commit containing a default unity project, where fastlane is initialized and the plugin "fastlane-plugin-unity-exporter" is added as relative path

---
+++ dev_fastlane.sh
@@ -0,0 +1,14 @@
+# expects the following hierarchy
+# root/ 
+#   |-- fastlane-plugin-unity-exporter/
+#   |-- fastlane-plugin-unity-exporter-package/
+#   |-- fastlane-plugin-unity-exporter-project/
+
+cd ..
+cd fastlane-plugin-unity-exporter/fastlane-plugin-unity_exporter
+rake release
+cd ..
+cd ..
+cd fastlane-plugin-unity-exporter-project/fastlane-unity-exporter/ios-export
+bundle exec fastlane install_plugins
+bundle exec fastlane custom_lane
