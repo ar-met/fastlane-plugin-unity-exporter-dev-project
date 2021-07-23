b7e2a1d1afac52fa9647d139211e19e83e94a7e7
steft <52441940+stef-t@users.noreply.github.com>
2021-07-23 14:04:44 +0200

    fixing path and lane name

--- dev_fastlane.sh
+++ dev_fastlane.sh
@@ -8,6 +8,6 @@ cd ..
 cd fastlane-plugin-unity-exporter
 rake release
 cd ..
-cd fastlane-plugin-unity-exporter-project/fastlane-unity-exporter/iOS
+cd fastlane-plugin-unity-exporter-project/fastlane-build-exporter/iOS
 bundle exec fastlane install_plugins
 bundle exec fastlane custom_lane
